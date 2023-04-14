f6d25c30dce4e18792d0224f32f9224ce356d6e3
Roman Kudryavtsev <kudryavtsevroman@mail.ru>
2023-04-15 01:57:05 +0300

    <fix: fix request controller test>

--- src/test/java/ru/practicum/shareit/request/RequestControllerTest.java
+++ src/test/java/ru/practicum/shareit/request/RequestControllerTest.java
@@ -4,7 +4,6 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,16 +12,18 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import ru.practicum.shareit.item.dto.ItemDto;
 import ru.practicum.shareit.request.dto.ItemRequestDto;
-import ru.practicum.shareit.request.model.ItemRequest;
+import ru.practicum.shareit.request.dto.ItemRequestWithItemsDto;
 import ru.practicum.shareit.request.service.RequestService;
-import ru.practicum.shareit.user.model.User;
-import ru.practicum.shareit.user.service.UserService;
 
 import java.nio.charset.StandardCharsets;
 import java.time.LocalDateTime;
+import java.util.List;
 
+import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.Matchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -35,19 +36,32 @@ public class RequestControllerTest {
     ObjectMapper mapper;
     @MockBean
     RequestService requestService;
-    @Mock
-    UserService userService;
     @Autowired
     private MockMvc mvc;
     private ItemRequestDto request;
+    private ItemRequestWithItemsDto requestWithItems;
+    private ItemDto item;
     private final LocalDateTime testNow = LocalDateTime.now();
 
     @BeforeEach
     void setUp() {
-        request = request.builder()
+        item = ItemDto.builder()
+                .id(1L)
+                .name("New item")
+                .description("New item requested")
+                .available(true)
+                .requestId(1L)
+                .build();
+        request = ItemRequestDto.builder()
                 .id(1L)
                 .description("New item needed")
                 .created(testNow)
+                .build();
+        requestWithItems = ItemRequestWithItemsDto.builder()
+                .id(1L)
+                .description("Request with items")
+                .created(testNow)
+                .items(List.of(item))
                 .build();
     }
 
@@ -66,17 +80,47 @@ public class RequestControllerTest {
     }
 
     @Test
-    void testGetUsersRequestWithItems() {
-
+    void testGetUsersRequestWithItems() throws Exception {
+        Mockito.when(requestService.getUsersRequestsWithItems(Mockito.anyLong())).thenReturn(List.of(requestWithItems));
+        mvc.perform(get("/requests")
+                        .header("X-Sharer-User-Id", "1"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", hasSize(1)))
+                .andExpect(jsonPath("$[0].id", is(requestWithItems.getId()), Long.class))
+                .andExpect(jsonPath("$[0].description", is(requestWithItems.getDescription())))
+                .andExpect(jsonPath("$[0].items", hasSize(1)))
+                .andExpect(jsonPath("$[0].items[0].id", is(item.getId()), Long.class))
+                .andExpect(jsonPath("$[0].items[0].requestId", is(requestWithItems.getId()), Long.class));
     }
 
     @Test
-    void testGetRequestByIdWithItems() {
-
+    void testGetRequestByIdWithItems() throws Exception {
+        Mockito.when(requestService.getRequestByIdWithItems(Mockito.anyLong(), Mockito.anyLong()))
+                .thenReturn(requestWithItems);
+        mvc.perform(get("/requests/1")
+                        .header("X-Sharer-User-Id", "1"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id", is(requestWithItems.getId()), Long.class))
+                .andExpect(jsonPath("$.description", is(requestWithItems.getDescription())))
+                .andExpect(jsonPath("$.items", hasSize(1)))
+                .andExpect(jsonPath("$.items[0].id", is(item.getId()), Long.class))
+                .andExpect(jsonPath("$.items[0].requestId", is(requestWithItems.getId()), Long.class));
     }
 
     @Test
-    void testGetAllRequestsOfOtherUsers() {
-
+    void testGetAllRequestsOfOtherUsers() throws Exception {
+        Mockito.when(requestService.getAllRequestsOfOtherUsers(Mockito.anyLong(), Mockito.anyInt(), Mockito.anyInt()))
+                .thenReturn(List.of(requestWithItems));
+        mvc.perform(get("/requests/all")
+                        .header("X-Sharer-User-Id", "1")
+                        .param("from", "0")
+                        .param("size", "10"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", hasSize(1)))
+                .andExpect(jsonPath("$[0].id", is(requestWithItems.getId()), Long.class))
+                .andExpect(jsonPath("$[0].description", is(requestWithItems.getDescription())))
+                .andExpect(jsonPath("$[0].items", hasSize(1)))
+                .andExpect(jsonPath("$[0].items[0].id", is(item.getId()), Long.class))
+                .andExpect(jsonPath("$[0].items[0].requestId", is(requestWithItems.getId()), Long.class));
     }
 }
