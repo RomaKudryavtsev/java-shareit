9305d52799e25b53c6aafd68b7c6c8ff0521bd37
Roman Kudryavtsev <kudryavtsevroman@mail.ru>
2023-04-15 00:32:39 +0300

    <feat: add user controller test>

---
+++ src/test/java/ru/practicum/shareit/user/UserControllerTest.java
@@ -0,0 +1,105 @@
+package ru.practicum.shareit.user;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import ru.practicum.shareit.user.model.User;
+import ru.practicum.shareit.user.service.UserService;
+
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+
+import static org.hamcrest.Matchers.*;
+import static org.mockito.Mockito.times;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@ExtendWith(MockitoExtension.class)
+@AutoConfigureMockMvc
+@SpringBootTest
+public class UserControllerTest {
+    @Autowired
+    ObjectMapper mapper;
+
+    @MockBean
+    UserService userService;
+
+    @Autowired
+    private MockMvc mvc;
+
+    private final User user = new User();
+
+    @BeforeEach
+    void setUp() {
+        user.setId(1L);
+        user.setName("John Sharer");
+        user.setEmail("js@gmail.com");
+    }
+
+    @Test
+    void testAddUser() throws Exception {
+        Mockito.when(userService.addUser(Mockito.any())).thenReturn(user);
+        mvc.perform(post("/users")
+                        .content(mapper.writeValueAsString(user))
+                        .characterEncoding(StandardCharsets.UTF_8)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id", is(user.getId()), Long.class))
+                .andExpect(jsonPath("$.name", is(user.getName())))
+                .andExpect(jsonPath("$.email", is(user.getEmail())));
+    }
+
+    @Test
+    void testUpdateUser() throws Exception {
+        user.setName("John A. Sharer");
+        Mockito.when(userService.updateUser(Mockito.anyLong(), Mockito.any())).thenReturn(user);
+        mvc.perform(patch("/users/1")
+                        .content(mapper.writeValueAsString(user))
+                        .characterEncoding(StandardCharsets.UTF_8)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id", is(user.getId()), Long.class))
+                .andExpect(jsonPath("$.name", is(user.getName())))
+                .andExpect(jsonPath("$.email", is(user.getEmail())));
+    }
+
+    @Test
+    void testGetUserById() throws Exception {
+        Mockito.when(userService.getUserById(Mockito.anyLong())).thenReturn(user);
+        mvc.perform(get("/users/1"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id", is(user.getId()), Long.class))
+                .andExpect(jsonPath("$.name", is(user.getName())))
+                .andExpect(jsonPath("$.email", is(user.getEmail())));
+    }
+
+    @Test
+    void testGetAllUsers() throws Exception {
+        Mockito.when(userService.getAllUsers()).thenReturn(List.of(user));
+        mvc.perform(get("/users"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", hasSize(1)))
+                .andExpect(jsonPath("$[0].id", is(user.getId()), Long.class))
+                .andExpect(jsonPath("$[0].name", is(user.getName())))
+                .andExpect(jsonPath("$[0].email", is(user.getEmail())));
+    }
+
+    @Test
+    void testDeleteUserById() throws Exception {
+        mvc.perform(delete("/users/1"))
+                .andExpect(status().isOk());
+        Mockito.verify(userService, times(1)).deleteUserById(1L);
+    }
+}
