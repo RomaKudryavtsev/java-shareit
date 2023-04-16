0e10c05850871dc82d288d17971f66e89ff49714
Roman Kudryavtsev <kudryavtsevroman@mail.ru>
2023-04-17 02:46:17 +0300

    <fix: delete redundant branches>

--- src/main/java/ru/practicum/shareit/item/model/Item.java
+++ src/main/java/ru/practicum/shareit/item/model/Item.java
@@ -52,16 +52,4 @@ public class Item {
     public Boolean getAvailable() {
         return available;
     }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof Item)) return false;
-        return id != null && id.equals(((Item) o).getId());
-    }
-
-    @Override
-    public int hashCode() {
-        return getClass().hashCode();
-    }
 }
