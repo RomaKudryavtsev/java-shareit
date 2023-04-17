5d56025c977aaae6a315f4fd78bd8698a023c01a
Roman Kudryavtsev <kudryavtsevroman@mail.ru>
2023-04-17 03:31:08 +0300

    <fix: update schema>

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -21,7 +21,7 @@ CREATE TABLE IF NOT EXISTS items (
   name VARCHAR(255) NOT NULL,
   description VARCHAR(512) NOT NULL,
   available BOOLEAN,
-  request_id BIGINT NOT NULL,
+  request_id BIGINT,
   CONSTRAINT pk_item PRIMARY KEY (id),
   CONSTRAINT fk_items_to_users FOREIGN KEY(owner_id) REFERENCES users(id),
   CONSTRAINT fk_items_to_requests FOREIGN KEY(request_id) REFERENCES requests(id)
