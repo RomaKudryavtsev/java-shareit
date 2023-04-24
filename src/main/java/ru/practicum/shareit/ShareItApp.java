995618e2b669b354e70316a5efff27d07c2eb178
Roman Kudryavtsev <kudryavtsevroman@mail.ru>
2023-04-24 15:51:09 +0300

    <refactor: pre-refactor commit>

--- src/main/java/ru/practicum/shareit/ShareItApp.java
+++ src/main/java/ru/practicum/shareit/ShareItApp.java
@@ -11,7 +11,7 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @EnableTransactionManagement
 @EntityScan
 public class ShareItApp {
-
+// Pre-refactor
     public static void main(String[] args) {
         SpringApplication.run(ShareItApp.class, args);
     }
