b0ad95ec0e7fd81f3d40b6548c1de4c9789d29ee
Roman Kudryavtsev <kudryavtsevroman@mail.ru>
2023-04-25 15:13:39 +0300

    <refactor: add gateway module>

--- shareIt-server/src/main/java/ru/practicum/shareit/ShareItApp.java
+++ shareIt-server/src/main/java/ru/practicum/shareit/ShareItApp.java
@@ -11,7 +11,6 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @EnableTransactionManagement
 @EntityScan
 public class ShareItApp {
-// Pre-refactor
     public static void main(String[] args) {
         SpringApplication.run(ShareItApp.class, args);
     }
